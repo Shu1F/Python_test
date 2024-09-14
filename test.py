47c2ca31caa2a046a2333c1b3a6f96bc453f6442
fs203 <fs2031@icloud.com>
2024-09-14 17:52:48 +0900

    new_lecture

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+# print("Hello World!")
+
+#演算
+
+# print(1+1)
+# print(1-1)
+# print(1*1)
+# print(10/2)
+# print(5%3)
+
+#変数
+
+hoge = 's_size'
+hoge_length = 0
+hoge_times = 5.5
+
+if hoge_length > 6:
+    print('ooi')
+elif hoge_length == 0:
+    print('nai!')
+else :
+    print('sukunai')
